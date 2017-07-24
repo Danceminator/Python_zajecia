c4a9b0243b542416531546e21c4ff4d145005073
Danceminator <danceminator@gmail.com>
2017-07-24 13:40:28 +0200

    uaktualnienie pliku

--- D1/Pyton-D.py
+++ D1/Pyton-D.py
@@ -91,3 +91,76 @@ print(round(-1.3), round(-1.5), round(-1.6))
 print(int(1.3), int(1.5), int(1.6))
 
 print(int(-1.3), int(-1.5), int(1.6))
+
+#p10
+
+'''kwota_br = float(input("podaj kwotę brutto, którą chcesz przeliczyć "))
+stawka=int(input("podaj stawkę VAT "))
+kwota_net = kwota_br/(1+(stawka/100))
+
+print("kwota netto przy stawce", str(stawka)+"%", "wynosi:", round(kwota_net, 2), "zł")'''
+
+#p11
+'''
+nazwa_p1 = "chleb"
+nazwa_p2 = "mleko"
+nazwa_p3 = "cukierek"
+
+cena_p1 = 1.99
+cena_p2=4.15
+cena_p3=15.99
+
+zamówienie_p1 = int(input("liczba chlebów: "))
+zamówienie_p2= float(input("litry mleka: "))
+zamówienie_p3=int(input("waga cukierków (g): "))
+
+suma= round((cena_p1*zamówienie_p1)+(cena_p2*zamówienie_p2)+((cena_p3/1000)*(zamówienie_p3)),2)
+print("Twoje zamówienie")
+print("=================")
+print("chleb:", zamówienie_p1, "szt.")
+print("mleko:", zamówienie_p2, "l")
+print("cukierki:", zamówienie_p3, "gram")
+print("=================")
+print("do zapłaty:", suma, "zł")
+'''
+
+
+a=12 +0j
+b=1+(-1j)
+print (a*b)
+
+log1=True
+print(type(log1))
+
+print(bool(""), bool (0), bool("ALA"))
+
+a = """
+autor
+data
+wersja
+"""
+print(a)
+
+#\n znak przejścia do nowej linii
+b="autor\ndata\nwersja"
+
+print(b)
+
+
+
+#cwiczenie dodatkowe
+
+'''imie=input("podaj imię: ")
+krotnosc= int(input("podaj ile razy ma być powtórzone Twoje imię: "))
+
+print((imie + ", ")*(krotnosc - 1)+imie+".")'''
+
+#P22
+
+Imie = input("podaj imię: ")
+Nazwisko = input("podaj nazwisko: ")
+wiek = input("podaj swój wiek: ")
+Stanowisko = input("podaj stanowisko ")
+Placa = input("podaj placę brutto: ")
+
+print ((Imie +" "+Nazwisko+" (wiek: "+ wiek+ " lat) pracuje na stanowisku: "+ Stanowisko+" (pensja: "+ Placa+ " brutto).\n")*10 )
