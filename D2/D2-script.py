015b0391beae61b19125657f4c7400928b6fd323
Danceminator <danceminator@gmail.com>
2017-07-25 12:58:38 +0200

    instrukcje warunkowe pętle

--- D2/D2-script.py
+++ D2/D2-script.py
@@ -27,13 +27,12 @@ to_rom={ 'jeden': "I", 'dwa': "II", 'trzy':"III", 'cztery':"IV", 'pięć':"V", '
 print (key1.capitalize()+"w postaci liczby rzymskiej to:"+(to_rom[key1]))'''
 
 #pkolejne
-
 '''key1=input('Podaj liczbę(1-5) zapispaną słownie: ')
 to_dec={ 'jeden':1, 'dwa': 2, 'trzy':3, 'cztery':4, 'pięć':5, 'piec':5}
-to_rom={ '1': "I", '2': "II", '3':"III", '4':"IV", '5':"V"}
+to_rom={ 1: "I", 2: "II", 3:"III", 4:"IV", 5:"V"}
 
-print (key1.capitalize() + " w postaci liczby dziesiętnej to: "+str(to_dec[key1])+",a w postaci rzymskiej: "+str(to_rom[to_dec[key1]]))'''
-
+print (key1.capitalize() + " w postaci liczby dziesiętnej to: "+str(to_dec[key1])+",a w postaci rzymskiej: "+str(to_rom[to_dec[key1]]))
+'''
 #p48
 '''nazwa= input("Jaki produkt chcesz zamówić? (chleb, bułki, bagietka):")
 ilosc=int(input("podaj ilość wybranego produktu:"))
@@ -62,7 +61,7 @@ print (kształty & okragle)
 print (kształty - okragle)
 print (kształty ^ okragle)'''
 
-los1=random.randint(1,49)
+'''los1=random.randint(1,49)
 
 print(los1)
 
@@ -78,4 +77,88 @@ S.add(random.randint(1,49))
 
 L=list(S)
 print (L[:6])
+'''
+# instrukcja warunkowa if
+
+'''a= int(input("podaj liczbę: "))
+if(a%2==0):
+    print("liczba "+str(a)+' jest parzysta')
+    if (a>=10):
+        print('liczba '+str(a)+' jest większa równa 10')
+    else:
+        print('liczba '+str(a)+' jest mniejsza od 10')
+     
+else:
+    print('liczba '+str(a)+' jest nieparzysta')
+    
+print ("jestem już za instrukcją if")'''
+
       
+'''elif(a>10): 
+print ("liczba "+str(a)+' jest wieksza od zera')'''
+
+
+# p53
+
+'''    print(bool(0)
+if (bool(1)):
+    print(bool(1)
+if (bool(2)):
+    print(bool(2)
+if (bool(3)):
+    print(bool(3)
+if (bool(4)):
+print (bool(4))'''
+
+#P54
+'''lista =[1,2,3,4,5,6,7,8,9]
+index = int(input("Podaj który element chcesz wyświetlić: "))
+             
+if(index >=0 and index <=(len(lista)-1)):
+    print('index is ok')
+    print (lista[index])
+else:
+    print ('out of rage')
+if (lista[0]>0 and lista[1]>0):
+    print ("oba elementy większe od zera")
+elif (lista[0]>0 and lista[1]<0):
+    print ("pierwszy element większy od zera a drugi mniejszy od zera")
+elif (lista[0]<0 and lista[1]>0):
+    print ("pierwszy element mniejszy od zera a drugi większy od zera")    
+else:
+    print ("oba elementy są mniejsze od zera") '''    
+
+'''A=set([1,2,3,4,])
+B= set([1,2,5,4])
+        
+if (A==B):
+    print ("zbiory są równe")
+elif (A.issubset(B)):
+    print("A jest podzbiorem B")
+elif (A.issuperset(B)):
+    print("B jest podzbiorem A")
+else:
+    print("zbiory są różne")'''
+
+
+lista =[1,2,3,4,5,6,7,8,9]
+
+i=len(lista)-1
+while (i>=0):
+        if(lista[i]%2==0):
+                print ("index: "+str(i)+"\t Wartosć: "+str(lista[i]))
+        i-=1
+else:
+        print("jestem w else")
+print ('poza pętlą')
+
+#która z liczb jest większa i o ile? wyrażenie trójargumentowe
+
+a = 20
+b=15
+print(" a jest większe od b o: "+str(a-b)) if (a >=b) else print("b jest większe od a o: "+str(b-a))
+
+
+
+
+
